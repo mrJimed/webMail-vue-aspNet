fb2ed26e3afd06d967b3426e7071837b13ac5885
Danil <danil0316@mail.ru>
2024-01-31 20:46:28 +0500

    add Header component

---
+++ webmail.client/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="py-5 border-b border-b-slate-200 shadow-sm">
+    <div class="flex justify-between w-5/6 mx-auto items-center">
+      <h1 class="font-bold text-2xl italic">WebMail</h1>
+
+      <div class="flex gap-4">
+        <button
+          type="button"
+          class="border border-black px-3 py-2 rounded-md transition hover:bg-black hover:text-white"
+        >
+          Регистрация
+        </button>
+        <button
+          type="button"
+          class="border border-black px-3 py-2 rounded-md transition hover:bg-black hover:text-white"
+        >
+          Вход
+        </button>
+      </div>
+    </div>
+  </header>
+</template>
