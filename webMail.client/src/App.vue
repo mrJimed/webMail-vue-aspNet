f247f02573e817ca26879835fb8835d402a5cafa
Danil <danil0316@mail.ru>
2024-07-19 21:38:07 +0500

    added header

--- webMail.client/src/App.vue
+++ webMail.client/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import Header from './components/Header.vue'
 import Home from './pages/Home.vue'
 </script>
 
 <template>
+  <Header></Header>
   <Home></Home>
 </template>
---
+++ webMail.client/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <header class="bg-green-500 py-2 select-none">
+    <div class="w-11/12 mx-auto">
+      <h1 class="font-bold text-xl">WebMail</h1>
+    </div>
+  </header>
+</template>
